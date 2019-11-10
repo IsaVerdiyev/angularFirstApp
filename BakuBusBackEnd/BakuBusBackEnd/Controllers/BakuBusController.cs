aecc752b1f8b7ee997eb0cd7803a7918dcaa276c
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-11-10 15:48:23 +0400

    Included bakubus backend

---
+++ BakuBusBackEnd/BakuBusBackEnd/Controllers/BakuBusController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace BakuBusBackEnd.Controllers
+{
+    [Route("api/buses")]
+    [ApiController]
+    public class BakuBusController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<String>> get()
+        {
+            HttpClient httpClient = new HttpClient();
+            
+            var json = await httpClient
+                .GetStringAsync("https://www.bakubus.az/az/ajax/apiNew1");
+            dynamic jtoken = JToken.Parse(json);
+            foreach(var item in jtoken.BUS)
+            {
+                item["@attributes"].LATITUDE = 
+                    Convert.ToString(item["@attributes"].LATITUDE).Replace(',', '.');
+                item["@attributes"].LONGITUDE =
+                    Convert.ToString(item["@attributes"].LONGITUDE).Replace(',', '.');
+            }
+            return JsonConvert.SerializeObject(jtoken);
+        }
+    }
+}
---
+++ BakuBusBackEnd/BakuBusBackEnd/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakuBusBackEnd.Models
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+}
