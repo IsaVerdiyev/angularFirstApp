d2803bbfbbe42ca4f28dc445eebd82c8b026ca74
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-11-11 20:41:10 +0400

    Changed logic of updating buses so now there is no blinking

--- frontendPart/src/app/components/baku-bus/baku-bus.component.ts
+++ frontendPart/src/app/components/baku-bus/baku-bus.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {AgmCoreModule} from '@agm/core'
-import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-baku-bus',
@@ -12,7 +10,7 @@ export class BakuBusComponent implements OnInit {
 
   constructor(private httpClient: HttpClient, private zone: NgZone, private ref: ChangeDetectorRef) { }
 
-  buses: Map<string, any> = new Map<string, any>();
+  buses = new Map<string, any>();
   lat = 40.4093;
   lng = 49.8671;
   zoom = 12;
@@ -27,39 +25,20 @@ export class BakuBusComponent implements OnInit {
 
 
   async getBuses(): Promise<void>{
-    let response = await this.httpClient.get<any>('https://localhost:44398/api/buses').toPromise();
+    let response = await this.httpClient.get<any>('https://localhost:5001/api/buses').toPromise();
     this.updateBuses(response.BUS);
   }
 
-
-  // async initializeBuses(){
-  //   let response = await this.httpClient.get<any>('https://localhost:44398/api/buses').toPromise();
-  //   for(let bus of response.BUS){
-  //     this.buses[bus['@attributes'].BUS_ID] = bus;
-  //     console.log(bus);
-  //   }
-  //   console.log(this.buses);
-  // }
-
    async updateBuses(recievedBuses:Array<any>){
     for(var bus of  recievedBuses){
-      
-      if(!this.buses.has(bus['@attributes'].BUS_ID)){
-        this.buses.set(bus['@attributes'].BUS_ID, bus);
-      }
+      if(!this.buses.has(bus['@attributes'].BUS_ID))
+        this.buses.set(bus['@attributes'].BUS_ID, bus);   
       else {
-        this.buses[bus['@attributes'].BUS_ID] = bus;
-        this.ref.markForCheck();
-        await this.delay(50);
-        console.log("here");
+        let current = this.buses.get(bus['@attributes'].BUS_ID);
+        current['@attributes'].LATITUDE = bus['@attributes'].LATITUDE;
+        current['@attributes'].LONGITUDE = bus['@attributes'].LONGITUDE; 
       }
     }
-
-    
-  }
-
-  delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
 }
